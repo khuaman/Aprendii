aed83059b7fc0b876a8d5b596da6b7b50aa17caf
khuaman <kevin.huaman@utec.edu.pe>
2023-09-24 03:06:18 -0500

    feat: add initial configuration

--- lambda_handler.py
+++
@@ -1,6 +0,0 @@
-import boto3
-
-
-def lambda_handler(event, context):
-
-    return
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from flask import Flask, jsonify, request
+import awsgi
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/', methods=['GET'])
+def GetMessage():
+    return jsonify({
+        "status": "SUCESS"
+    })
